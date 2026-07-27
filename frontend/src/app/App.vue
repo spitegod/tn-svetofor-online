2d0ec84e0d866f2b997d26b14ecf93cf8133c82c
spitegod <iermakov2207@gmail.com>
2026-07-27 15:48:11 +0300

    Сворачивать блок фильтров после выбора типа строительства

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -123,6 +123,9 @@ let systemDocumentRequestController: AbortController | null = null
 let documentTableRequestController: AbortController | null = null
 const selectedSystemTypeSlug = ref('')
 const isSystemTypesOpen = ref(false)
+const isChangesFiltersOpen = ref(true)
+const isSystemsFiltersOpen = ref(true)
+const isClassificationPrimaryFiltersOpen = ref(true)
 const selectedHistorySystem = ref<SystemDocumentRow | null>(null)
 const systemHistoryRows = ref<SystemDocumentRow[]>([])
 const isSystemHistoryLoading = ref(false)
@@ -1678,6 +1681,13 @@ function systemTypeImageSource(type: SystemTypeOption) {
 function selectConstructionType(type: string) {
   selectedConstructionType.value = type
   isSystemTypesOpen.value = false
+  if (activePage.value === 'changes') {
+    isChangesFiltersOpen.value = false
+  } else if (activePage.value === 'systems') {
+    isSystemsFiltersOpen.value = false
+  } else if (activePage.value === 'classification') {
+    isClassificationPrimaryFiltersOpen.value = false
+  }
   classificationPage.value = 1
   systemDocumentPage.value = 1
   classificationCatalogPage.value = 1
@@ -2543,6 +2553,7 @@ const classificationPageModel: ClassificationPageViewModel = {
   hasActiveClassificationPageFilters,
   hideBrokenSystemTypeImage,
   isClassificationCatalogLoading,
+  isClassificationPrimaryFiltersOpen,
   isClassificationSearchPending,
   isSystemTypesOpen,
   openedClassificationFilter,
@@ -2588,6 +2599,7 @@ const systemsPageModel: SystemsPageViewModel = {
   isBulkComparisonUpdating,
   isSystemDocumentLoading,
   isSystemFiltering,
+  isSystemsFiltersOpen,
   isSystemTypesOpen,
   isSystemsRefreshDone,
   isSystemsRefreshing,
@@ -2651,6 +2663,7 @@ const changesPageModel: ChangesPageViewModel = {
   filterChangesByClass,
   hasActiveChangeFilters,
   isChangesRefreshDone,
+  isChangesFiltersOpen,
   isChangesRefreshing,
   isClassificationFiltering,
   isClassificationLoading,
